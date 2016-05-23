153457b8a6cf4681e01788d94599d85723bfac51
Spyispie120 <ducnguyenduchao@gmail.com>
2016-05-22 19:13:56 -0700

    -Add Easing class

---
+++ core/src/com/picklegames/Easing.java
@@ -0,0 +1,35 @@
+package com.picklegames;
+
+public class Easing {
+	public static float easeLinear (float t,float b , float c, float d) {
+		return c*t/d + b;
+	}
+	
+	public static float  easeInQuad(float t,float b , float c, float d) {
+		return c*(t/=d)*t + b;
+	}
+	
+	public static float easeInCubic (float t,float b , float c, float d) {
+		return c*(t/=d)*t*t + b;
+	}
+	
+	public static float  easeInQuart(float t,float b , float c, float d) {
+		return c*(t/=d)*t*t*t + b;
+	}
+	
+	public static float easeInQuint (float t,float b , float c, float d) {
+		return c*(t/=d)*t*t*t*t + b;
+	}
+	
+	public static float  easeInCos(float t,float b , float c, float d) {
+		return -c * (float)Math.cos(t/d * (Math.PI/2)) + c + b;
+	}
+	
+	public static float  easeOutSin(float t,float b , float c, float d) {
+		return c * (float)Math.sin(t/d * (Math.PI/2)) + b;	
+	}
+	
+	public static float  easeInExpo(float t,float b , float c, float d) {
+		return (t==0) ? b : c * (float)Math.pow(2, 10 * (t/d - 1)) + b;
+	}
+}
--- core/src/com/picklegames/demo/TweenEaseDemo.java
+++ core/src/com/picklegames/demo/TweenEaseDemo.java
@@ -8,25 +8,29 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.picklegames.Easing;
 import com.picklegames.shape.Circle;
 import com.picklegames.shape.Shape;
 
 public class TweenEaseDemo implements InputProcessor {
 	private ShapeRenderer sr;
-	private Shape cir1;
 	private List<Shape> shapes;
+	private boolean isTween = false;
+	private Shape cir1;
 
 	private final float EASE = .1f;
-
+	private final float DURATION = 1f;
+	private Vector2 originalPosition = new Vector2();
 
 	public TweenEaseDemo() {
 		sr = new ShapeRenderer();
-		cir1 = new Circle(100, 100, 5);
+
+		cir1 = new Circle(0, 0, 5);
+		originalPosition.set(cir1.getPosition());
 
 		shapes = new ArrayList<Shape>();
-		shapes.add(cir1);
-		for (int i = 0; i < 100; i++) {
-			Shape s = new Circle(100, 100, 5);
+		for (int i = 0; i < 1; i++) {
+			Shape s = new Circle(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 5);
 			shapes.add(s);
 		}
 
@@ -35,24 +39,45 @@ public class TweenEaseDemo implements InputProcessor {
 
 	private Vector2 mousePosition = new Vector2();
 	private Vector2 mouseClick = new Vector2();
-	private Vector2 leader = new Vector2();
+	private Vector2 target = new Vector2();
+	private float timeElapsed;
+	float changeX;
+	float changeY;
 
 	public void update(float dt) {
 		mousePosition.set(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
-		leader = mouseClick;
+		target = mouseClick;
 
-		for (int i = 0; i < shapes.size(); i++) {
-			easeTo(shapes.get(i), leader);
-			leader = shapes.get(i).getPosition();
+		System.out.println(timeElapsed);
+		System.out.println(changeX + " " + changeY);
+		if (isTween) {
+			timeElapsed += dt;
+
+			if (timeElapsed < DURATION) {
+
+				cir1.setPosition(easeTo(timeElapsed, originalPosition.x, changeX, DURATION),
+						easeTo(timeElapsed, originalPosition.y, changeY, DURATION));
+
+			} else {
+				timeElapsed = 0;
+				cir1.setPosition(target.x, target.y);
+				originalPosition.set(target);
+				isTween = false;
+
+			}
 		}
-
 	}
 
 	public void render() {
-		for (Shape s : shapes) {
-			s.render(sr);
-		}
 		cir1.render(sr);
+
+		// for (Shape s : shapes) {
+		// s.render(sr);
+		// }
+	}
+
+	public float easeTo(float t, float b, float c, float d) {
+		return Easing.easeInCos(t, b, c, d);
 	}
 
 	public void easeTo(Shape shape, Vector2 target) {
@@ -89,8 +114,12 @@ public class TweenEaseDemo implements InputProcessor {
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 		if (button == Input.Buttons.LEFT) {
-			leader = mouseClick.set(screenX, Gdx.graphics.getHeight() - screenY);
-
+			if (!isTween) {
+				target = mouseClick.set(screenX, Gdx.graphics.getHeight() - screenY);
+				changeX = target.x - originalPosition.x;
+				changeY = target.y - originalPosition.y;
+				isTween = true;
+			}
 			return true;
 		}
 		return false;
